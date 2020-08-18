3808bb19f3e3ce11a38eb6b55fa32d4017d6ba8b
孔佳齐丶 <kjqjava@163.com>
2020-08-18 15:27:50 +0800

    SeataOrderMainApp2001的Module

---
+++ seata-order-service2001/src/main/java/com/kjq/springcloud/alibaba/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.kjq.springcloud.alibaba.service;
+
+import com.kjq.springcloud.alibaba.domain.Order;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-26 15:19
+ */
+public interface OrderService
+{
+    void create(Order order);
+}
---
+++ seata-order-service2001/src/main/java/com/kjq/springcloud/alibaba/service/StorageService.java
@@ -0,0 +1,17 @@
+package com.kjq.springcloud.alibaba.service;
+
+import com.kjq.springcloud.alibaba.domain.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-26 15:22
+ */
+@FeignClient(value = "seata-storage-service")
+public interface StorageService
+{
+    @PostMapping(value = "/storage/decrease")
+    CommonResult decrease(@RequestParam("productId") Long productId, @RequestParam("count") Integer count);
+}
